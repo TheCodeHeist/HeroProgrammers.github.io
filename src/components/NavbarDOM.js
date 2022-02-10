a61e5202e9d1e8b725978c1eb58091671bd59b76
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2022-02-10 17:51:46 +0600

    Responsive Navbar Added...

--- src/components/NavbarDOM.js
+++ src/components/NavbarDOM.js
@@ -2,18 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./../styles/NavbarDOM.css";
 import logogif from "./../assets/logo.gif";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function NavbarDOM(props) {
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <nav>
-      <div className="navbar-header">
-        <img src={logogif} alt="logo" className="logo" />
-        <Link to="/" className="navbar-brand" href="index.html">
-          Hero Programmers
-        </Link>
+      <div className="top">
+        <div className="navbar-header">
+          <img src={logogif} alt="logo" className="logo" />
+          <Link to="/" className="navbar-brand">
+            Hero Programmers
+          </Link>
+        </div>
+
+        <button type="button" className="navbar-toggler" onClick={toggleNav}>
+          <FontAwesomeIcon icon={faBars} />
+        </button>
       </div>
 
-      <div className="navbar-navigation">
+      <div className={`navbar-navigation ${isNavOpen ? "nav-active" : ""}`}>
         <Link
           to="/"
           className={
